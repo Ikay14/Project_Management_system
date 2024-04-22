6b46914e2f99bc90bdac3d1c480537dcbd9d63a7
Ikay14 <divinevictoryihemegbulem.com>
2024-04-22 12:04:23 +0100

    Deleted the utils files

--- utils/jwtUtils.js
+++
@@ -1,9 +0,0 @@
-// const jwt = require('jsonwebtoken');
-
-// const generateToken = (userId) => {
-//   const secretKey = 'sihenceBoyCodes'
-//   const token = jwt.sign({ userId }, secretKey, { expiresIn: '1h' }); 
-//   return token;
-// };
-
-// module.exports = { generateToken };
